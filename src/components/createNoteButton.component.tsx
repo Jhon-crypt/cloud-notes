355d7eda482cdde97982901d8bb64f4395864250
Jhon-crypt <johnoladele690@gmail.com>
2023-03-02 21:48:20 +0100

    Added note button

--- src/components/createNoteButton.component.tsx
+++ src/components/createNoteButton.component.tsx
@@ -1,22 +1,23 @@
 import React from 'react'
-import {FiPlusCircle} from "react-icons/fi";
+import { FiPlusCircle } from "react-icons/fi";
 
-function CreateNoteButton(){
+function CreateNoteButton() {
 
     return (
 
         <>
-        
+
             <div className="px-4 pt-3 my-5 text-center mx-auto" style={{ "maxWidth": "330px" }}>
 
-                <button className="w-100 btn btn-lg" style={{ "backgroundColor" : "#F7DD88","color" : "#38568C" }}>
+                <button className="w-100 btn btn-lg" style={{ "backgroundColor": "#F7DD88", "color": "#38568C" }} 
+                data-bs-toggle="modal" data-bs-target="#createNoteModal">
 
                     Create Notes <FiPlusCircle />
 
                 </button>
 
             </div>
-        
+
         </>
 
     )
